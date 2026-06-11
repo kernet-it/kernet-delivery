c40dd5d5d0171435108fc6fa5881ff2f87faa987
Hodei Navarro <hnavarro@kernet.es>
2026-06-11 17:16:05 +0200

    Bootstrap 18.0 from addon-repo-template (copier)

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+
+const config = {
+    bracketSpacing: true,
+    printWidth: 88,
+    proseWrap: "always",
+    semi: true,
+    trailingComma: "es5",
+    arrowParens: "avoid",
+    bracketSameLine: true,
+    tabWidth: 4,
+};
+
+module.exports = config;
